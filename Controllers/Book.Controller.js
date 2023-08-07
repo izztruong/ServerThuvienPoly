9ff7bbfe252939f2a22ba88aa6bb68047401b13b
izztruong <118030005+izztruong@users.noreply.github.com>
2023-08-07 21:46:11 +0700

    Update Book.Controller.js

--- Controllers/Book.Controller.js
+++ Controllers/Book.Controller.js
@@ -1,18 +1,44 @@
 const bookModel = require("../Model/Books.Model");
+const {
+  mutipleMongoosetoObject,
+  MongoosetoObject,
+} = require("../Util/mongoUtil");
+const BookController = require("./Book.Controller");
+
 const cloudinary = require("cloudinary").v2;
 
 class BookController {
-  async getBookbyId(req, res, next) {
-    const book = await bookModel
-      .findById(req.params.idBook)
-      .populate("categoryBook");
-    if (!book) {
-      return res.status(404).json({ message: "Book not found" });
-    }
-    res.status(200).json(book);
+  async getlist(req, res) {
+    await bookModel
+      .find()
+      .populate("categoryBook")
+      .then((book) => {
+        res.send({
+          book: mutipleMongoosetoObject(book),
+        });
+      });
   }
-
-  async addBook(req, res, next) {
+  async getlist10(req, res) {
+    bookModel
+      .find()
+      .limit(10)
+      .then((book) => {
+        res.json({
+          book: mutipleMongoosetoObject(book),
+        });
+      });
+  }
+  async getBookById(req, res, next) {
+    await bookModel
+      .findById(req.params.idBook)
+      .populate("categoryBook")
+      .then((book) => {
+        res.send({
+          book: MongoosetoObject(book),
+        });
+      });
+  }
+  async addBook(req, res) {
     const book = req.body;
     const img = req.file;
     const publishingYear = req.body.publishingYear;
@@ -50,119 +76,66 @@ class BookController {
       return res
         .status(500)
         .json({ message: "publishingYear không được lớn hơn ngày hôm nay" });
-    }
-    book.loanCount = 0;
-    if (!img == undefined) {
-      book.image = img?.path;
-    }
+    } else {
+      book.loanCount = 0;
+      if (img !== undefined) {
+        book.image = img?.path;
+      }
 
-    const items = new bookModel(book);
-    try {
-      console.log(items);
-      await items.save();
-      res.send(items);
-    } catch (error) {
-      console.log(error);
+      const items = new bookModel(book);
+
+      try {
+        console.log(items);
+        await items.save().then((book) => {
+          res.send({
+            book: MongoosetoObject(book),
+          });
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
   async deleteBook(req, res, next) {
     const idBook = req.params.idBook;
-    const deleteBook = await bookModel.findByIdAndDelete(idBook);
-    if (!deleteBook) {
-      return res.status(404).json({ message: "Book not found" });
-    }
-    res.json("delete success");
+    await bookModel
+      .findByIdAndDelete(idBook)
+      .then(() => {
+        res.status(200).json({ message: "Book deleted successfully" });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
   }
 
-  async updateBook(req, res, next) {
+  async updateBook(req, res) {
     const bookid = req.params.idBook;
     const book = req.body;
     const img = req.file;
-    const publishingYear = req.body.publishingYear;
-    let publishingYeardate = new Date(publishingYear);
-    let today = new Date();
-    console.log("publishingYear" + publishingYeardate);
-    console.log("today" + today);
-
-    if (
-      (img == undefined && book.image == "") ||
-      book.nameBook == "" ||
-      book.categoryBook == "" ||
-      book.author == "" ||
-      book.publishingCompany == "" ||
-      book.publishingYear == "" ||
-      book.language == "" ||
-      book.quanity == "" ||
-      book.price == ""
-    ) {
-      if (img) {
-        cloudinary.uploader.destroy(img.filename);
-      }
-      return res
-        .status(500)
-        .json({ message: "Các trường không được bỏ trống" });
-    } else if (!publishingYeardate || isNaN(publishingYeardate.getTime())) {
-      if (img) {
-        cloudinary.uploader.destroy(img.filename);
-      }
-      return res.status(500).json({ message: "publishingYear không hợp lệ" });
-    } else if (publishingYeardate > today) {
-      if (img) {
-        cloudinary.uploader.destroy(img.filename);
-      }
-      return res
-        .status(500)
-        .json({ message: "publishingYear không được lớn hơn ngày hôm nay" });
-    }
     cloudinary.uploader.destroy(book.image);
-    if (!img == undefined) {
+    if (img != undefined) {
       book.image = img?.path;
     }
-    const updateBook = await bookModel.findOneAndUpdate(
-      { _id: bookid },
-      req.body
-    );
-    if (!updateBook) {
-      return res.status(404).json({ message: "Book not found" });
-    }
-    res.status(200).json(book);
-  }
 
-  // lọc theo thể loại
+    await bookModel
+      .findOneAndUpdate({ _id: bookid }, book)
+      .then(() => {
+        res.status(200).json({ message: "Update thành công" });
+      })
+      .catch((err) => {
+        cloudinary.uploader.destroy(img.filename);
+        res.status(500).json({ error: err });
+        console.log(err);
+      });
+  }
   async getBookFollowCategoryBook(req, res, next) {
     const categoryBook = req.params.categoryBook;
-    const books = await bookModel.find({ categoryBook: categoryBook });
-    if (!books) {
-      return res.status(404).json({ message: "Category not found" });
-    }
-    res.status(200).json(books);
-  }
-
-  async listBook(req, res, next) {
-    try {
-      const arrBook = await bookModel.find().populate("categoryBook");
-      res.send(arrBook);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async listBook10(req, res, next) {
-    try {
-      const arrBook = await bookModel
-        .find()
-        .populate("categoryBook")
-        .sort({ loanCount: -1 })
-        .limit(10);
-      res.send(arrBook);
-    } catch (error) {
-      console.log(error);
-    }
+    await bookModel.find({ categoryBook: categoryBook }).then((book) => {
+      res.send({
+        book: mutipleMongoosetoObject(book),
+      });
+    });
   }
 }
 module.exports = new BookController();
-// exports.top10 = async (req, res, next) => {
-//   const arrBook = await bookModel.find();
-//   res.send(arrBook);
-// };
